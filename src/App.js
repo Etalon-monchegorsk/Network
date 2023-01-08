685867794d42d7a6c00c9145e55b50033946ceb8
Etalon-monchegorsk <bitnit633@gmail.com>
2023-01-08 19:25:54 +0300

    Design_vk_network

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Profile from './components/Profile/Profile';
+import { Routes, Route } from 'react-router-dom';
+import Messages from './components/Messages/Messages';
+
 
 function App() {
   return (
     <div className="App">
-        <Header />
-        <Nav />
-        <Profile />
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/messages' element={<Messages />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+
+
 
 function Nav() {
   return (
     <div className='Nav'>
-        Nav
+        <Link to='/profile'>Profile</Link>
+        <Link to='/messages'><FontAwesomeIcon icon={solid('user-secret')} />Message</Link>
+
     </div>
-  )
+  )  
 }
 
 export default Nav;
